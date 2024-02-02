9c3ba5346a6da1258cbce4632d22c6eaf27ec2fb
Matthew Wagerfield <matthew@wagerfield.com>
2024-02-02 23:03:02 +0000

    feat: add unscene dir

---
+++ src/unscene/math.ts
@@ -0,0 +1,7 @@
+export const add = (a: number, b: number) => a + b
+
+export const sub = (a: number, b: number) => a - b
+
+export const mul = (a: number, b: number) => a * b
+
+export const div = (a: number, b: number) => a / b
