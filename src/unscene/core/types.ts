094d846a6e5f2f399e69f5601b84a5ec064a2f14
Matthew Wagerfield <matthew@wagerfield.com>
2024-02-03 00:18:55 +0000

    feat: add core types

---
+++ src/unscene/core/types.ts
@@ -0,0 +1,13 @@
+export type AnyFunction = (...args: any[]) => any
+
+export type AnyObject = Record<string, any>
+
+export type AnyArray = any[]
+
+export type Nil = null | undefined
+
+export type FillStyle = null | string
+
+export type LineJoin = CanvasLineJoin
+
+export type LineCap = CanvasLineCap
