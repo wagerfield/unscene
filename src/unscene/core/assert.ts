67fccf4ab9f164bae9070a8ba875de2204f05223
Matthew Wagerfield <matthew@wagerfield.com>
2024-02-03 00:20:02 +0000

    feat: add assert utils

---
+++ src/unscene/core/assert.ts
@@ -0,0 +1,19 @@
+import { isNull, isUndefined } from "./guard"
+import type { Nil } from "./types"
+
+export function assertUndefined<T>(
+  value: T | undefined,
+  error = "value undefined"
+): T {
+  if (!isUndefined(value)) return value
+  throw new Error(error)
+}
+
+export function assertNull<T>(value: T | null, error = "value is null"): T {
+  if (!isNull(value)) return value
+  throw new Error(error)
+}
+
+export function assertNil<T>(value: T | Nil, error?: string): T {
+  return assertUndefined(assertNull(value, error), error)
+}
